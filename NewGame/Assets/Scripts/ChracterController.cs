b6be8d0a105683ade0a36ebc18b671b99d9519d7
minsku21 <97434687+minsku21@users.noreply.github.com>
2022-04-25 23:21:52 +0300

    game ready

--- NewGame/Assets/Scripts/ChracterController.cs
+++ NewGame/Assets/Scripts/ChracterController.cs
@@ -8,6 +8,10 @@ public class ChracterController : MonoBehaviour
     public float runSpeed = 40;
     Vector2 horizontalMove;
     public float jumpForce = 500;
+    bool IsGrounded;
+    public Transform groundcheck;
+    public LayerMask groundlayer;
+
 
 
     // Start is called before the first frame update
@@ -22,13 +26,14 @@ public class ChracterController : MonoBehaviour
         horizontalMove.x = Input.GetAxis("Horizontal") * runSpeed; 
         horizontalMove.y = rb.velocity.y;
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetKeyDown(KeyCode.Space)&&IsGrounded)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * jumpForce);
         }
+        rb.velocity = horizontalMove;
     }
     void FixedUpdate()
     {
-        rb.velocity = horizontalMove;
+        IsGrounded = Physics2D.OverlapCircle(groundcheck.position, 0.2f, groundlayer);
     }
 }
